f9d9652fb9bc87755be9593f321d8171b8a106b5
Luis Capuyon <frostehhh967@gmail.com>
2020-10-13 12:53:24 +0800

    Angular - Updated

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,112 +1,90 @@
 import { Component, ViewChild } from '@angular/core';
 import { DxDataGridComponent, DxPopupComponent, DxFormComponent } from 'devextreme-angular';
+import DataSource from 'devextreme/data/data_source';
+import ArrayStore from 'devextreme/data/array_store'
+import { Customer, Service } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Service]
 })
 export class AppComponent {
-    title = 'app';
-
     @ViewChild("grid", {static: false}) grid: DxDataGridComponent;
     @ViewChild("popup", {static: false}) popup: DxPopupComponent;
     @ViewChild("form", {static: false}) form: DxFormComponent;
+    key: string = "ID";
     mode: string = "";
     formData: any = {};
-    popupTitle: string = "Add";
+    popupMode: string = "Add";
     popupVisible: boolean = false;
+    customers: Customer[];
+    customerStore: ArrayStore;
+    gridSource: DataSource;
 
-    constructor() {
+    constructor(service: Service) {
+        this.customers = service.getCustomers();
+        this.customerStore = new ArrayStore({
+            data: this.customers,
+            key: this.key
+        })
+        this.gridSource = new DataSource({
+            store: this.customerStore
+        })
+
+        this.addClick = this.addClick.bind(this);
         this.editClick = this.editClick.bind(this);
         this.confirmClick = this.confirmClick.bind(this);
         this.cancelClick = this.cancelClick.bind(this);
-    }
-
-    editClick(e) {
-        this.showPopup("edit", e.row.data);
+        this.onToolbarPreparing = this.onToolbarPreparing.bind(this);
     }
 
     onToolbarPreparing(e) {
         let toolbarItems = e.toolbarOptions.items;
 
-        toolbarItems.forEach((item) => {
+        // customize addRow toolbar button
+        for (let i = 0; i < toolbarItems.length; i++) {
+            let item = toolbarItems[i];
             if (item.name === "addRowButton") {
-                item.options.onClick = (x) => {
-                    this.showPopup("add", undefined);
-                }
-            }
-        });
-    }
-
-    showPopup(popupMode, data) {
-        this.mode = popupMode;
-        this.formData = data !== undefined ? data : {};
-        
-        this.popupTitle = popupMode === "add" ? "Add" : "Edit";
-
-        this.popupVisible = true;
-    }
-
-    confirmClick(e) {
-        let form = this.form.instance;
-        let result = form.validate();
-        let gridSource = this.grid.instance.getDataSource();
-        let data = this.formData;
-        if (this.mode === "edit") {
-            if (result.isValid) {
-                console.log("edit", data);
-                gridSource.store().push([
-                    {
-                        type: "update",
-                        data: data,
-                        key: data["ID"]
-                    }
-                ]);
-                gridSource.reload();
-
-                this.formData = {};
-
-                this.popup.instance.hide();
-            }
-        } else if (this.mode === "add") {
-            if (result.isValid) {
-                console.log("add", data);
-                gridSource.store().push([
-                    {
-                        type: "insert",
-                        data: data
-                    }
-                ]);
-                gridSource.reload();
-
-                this.formData = {};
-                
-                this.popup.instance.hide();
+                item.options.onClick = this.addClick;
+                break;
             }
         }
     }
 
-    cancelClick(e) {
-        this.popup.instance.hide();
+    showPopup(popupMode, data) {
+        this.formData = data !== undefined ? data : {};
+        this.popupMode = popupMode;
+        this.popupVisible = true;
     }
 
-    source: any[] = [{
-        "ID": 1,
-        "CompanyName": "Super Mart of the West",
-        "Zipcode": 72716,
-        "Website": "http://www.nowebsitesupermart.com"
-    }, {
-        "ID": 2,
-        "CompanyName": "Electronics Depot",
-        "Zipcode": 30339,
-        "Website": "http://www.nowebsitedepot.com"
-    }, {
-        "ID": 3,
-        "CompanyName": "K&S Music",
-        "Zipcode": 55403,
-        "Website": "http://www.nowebsitemusic.com"
-    }];
-    
+    confirmClick(e) {
+        let result = this.form.instance.validate();
+        if (result.isValid) {
+            let gridSource = this.gridSource,
+                gridStore = this.customerStore;
 
+            if (this.popupMode === "Add")
+                gridStore.push([{ type: "insert", data: this.formData }]);
+            else if (this.popupMode === "Edit") {
+                gridStore.push([{ type: "update", data: this.formData, key: this.formData[this.key]}]);
+            }
+
+            gridSource.reload();
+            this.popupVisible = false;
+        }
+    }
+
+    addClick(e) {
+        this.showPopup("Add", undefined);
+    }
+
+    editClick(e) {
+        this.showPopup("Edit", {...e.row.data});
+    }
+
+    cancelClick(e) {
+        this.popupVisible = false;
+    }
 }
