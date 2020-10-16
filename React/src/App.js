8cbae42d4d6d61d142b310be514c4908e6c5b42c
Luis Capuyon <frostehhh967@gmail.com>
2020-10-16 17:51:48 +0800

    Added react - incomplete

---
+++ React/src/App.js
@@ -0,0 +1,170 @@
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import React, { useContext, useMemo, useState } from 'react';
+import './App.css';
+import { DataGrid, Editing, Column, Button } from 'devextreme-react/data-grid'
+import { Popup, ToolbarItem } from 'devextreme-react/popup'
+import { Form, SimpleItem } from 'devextreme-react/form'
+import {
+  RequiredRule,
+} from 'devextreme-react/validator';
+import { customers } from './data.js';
+import DataSource from 'devextreme/data/data_source';
+import ArrayStore from 'devextreme/data/array_store'
+
+const key = "ID";
+let customerStore = new ArrayStore({
+  data: customers,
+  key: key
+})
+
+// const gridSourceContext = React.createContext(gridSource)
+
+const colCountByScreen = { 
+  lg: 2, md: 2, sm: 1, xs: 1
+}
+const formRef = React.createRef(),
+  getForm = () => {
+    return formRef.current.instance;
+  }
+
+function App() {
+  // CHANGE TO DISPATCH
+  const [formData, setFormData] = useState({});
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [popupMode, setPopupMode] = useState("");
+  const [gridSource, setGridSource] = useState(new DataSource({
+    store: customerStore
+  }))
+  // const gridSource = useContext(gridSourceContext);
+  // const gridSource = gridSource
+  
+  const confirmBtnOptions = useMemo(() => { 
+    return { 
+      text: 'Confirm', 
+      type: 'success',
+      onClick: confirmClick 
+    }
+  }, []);
+  const cancelBtnOptions = useMemo(() => { 
+    return { 
+      text: 'Cancel', 
+      onClick: cancelClick 
+    }
+  }, []);
+
+  function onToolbarPreparing(e) {
+    let toolbarItems = e.toolbarOptions.items;
+  
+    // customize addRow toolbar button
+    for (let i = 0; i < toolbarItems.length; i++) {
+        let item = toolbarItems[i];
+        if (item.name === "addRowButton") {
+            item.options.onClick = addClick;
+            break;
+        }
+    }
+  }
+
+  function editClick(e) {
+    showPopup("Edit", {...e.row.data});
+  }
+
+  function addClick(e) {
+    showPopup("Add", undefined);
+  }
+
+  function confirmClick(e) {
+    let result = getForm().validate();
+    if (result.isValid) {
+        if (popupMode === "Add")
+          customerStore.push([{ type: "insert", data: formData }]);
+        else if (popupMode === "Edit") 
+          customerStore.push([{ type: "update", data: formData, key: formData[key]}]);
+        
+          debugger;
+        // setPopupVisible(false)
+        setGridSource(new DataSource({
+          store: customerStore
+        }))
+        gridSource.reload();
+        
+    }
+  }
+
+  function cancelClick(e) {
+    setPopupVisible(false)
+  }
+
+
+  function showPopup(popupMode, data) {
+    // CHANGE TO DISPATCH
+    setFormData(data !== undefined ? data : {})
+    setPopupMode(popupMode);
+    setPopupVisible(true);
+  }
+
+  return (
+    <React.Fragment>
+      <DataGrid
+        dataSource={gridSource}
+        onToolbarPreparing={onToolbarPreparing}
+      >
+        <Editing 
+          allowUpdating={true}
+          allowAdding={true}
+          allowDeleting={true}
+          useIcons={true}
+          mode="popup"
+        />
+        <Column dataField="ID" dataType="number"/>
+        <Column dataField="CompanyName" dataType="string"/>
+        <Column dataField="Zipcode" dataType="number"/>
+        <Column dataField="Website" dataType="string"/>
+        <Column type="buttons">
+            <Button name="edit" onClick={editClick} />
+            <Button name="delete" />
+        </Column>
+      </DataGrid>
+      <Popup 
+        title={popupMode}
+        closeOnOutsideClick={true}
+        visible={popupVisible}>
+          <ToolbarItem 
+            widget="dxButton"
+            location="after"
+            toolbar="bottom"
+            options={confirmBtnOptions}
+          />
+          <ToolbarItem 
+            widget="dxButton"
+            location="after"
+            toolbar="bottom"
+            options={cancelBtnOptions}
+          />
+          <Form 
+            ref={formRef}
+            formData={formData}
+            labelLocation="top"
+            showColonAfterLabel={true}
+            colCountByScreen={colCountByScreen}>
+            <SimpleItem dataField="ID">
+              <RequiredRule />
+            </SimpleItem>
+            <SimpleItem dataField="CompanyName">
+              <RequiredRule />
+            </SimpleItem>
+            <SimpleItem dataField="Zipcode">
+              <RequiredRule />
+            </SimpleItem>
+            <SimpleItem dataField="Website">
+              <RequiredRule />
+            </SimpleItem>
+          </Form>
+        </Popup>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
