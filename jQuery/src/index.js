faac56a1d4c065b3b4c819024674df67b0f0691a
CORP\serhii.pylypchuk <serhii.pylypchuk@devexpress.com>
2023-04-11 18:28:36 +0400

    Changes after review

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,237 +1,243 @@
 $(function () {
-    let rowData = {},
-        formMode,
-        form;
-
     const validationRules = {
-        firstName: [
-            { type: 'required', message: 'First Name is required.' }
-        ],
-        lastName: [
-            { type: 'required', message: 'Last Name is required.' }
-        ],
-        phone: [
-            { type: 'required', message: 'Phone number is required.' }
-        ],
-        email: [
-            { type: 'email', message: 'Email is incorrect.' }
-        ],
+        firstName: [{ type: "required", message: "First Name is required." }],
+        lastName: [{ type: "required", message: "Last Name is required." }],
+        phone: [{ type: "required", message: "Phone number is required." }],
+        email: [{ type: "email", message: "Email is incorrect." }],
         birthDate: [
-            { 
-                type: 'required', 
-                message: 'Birth Date is required.', 
-                invalidDateMessage: 'The date must have the following format: mm/dd/yyyy'
+            {
+                type: "required",
+                message: "Birth Date is required.",
+                invalidDateMessage:
+                    "The date must have the following format: mm/dd/yyyy",
             },
         ],
     };
 
-    const labelTemplate = (iconName) => 
-        (data) => $(`<div><i class='dx-icon dx-icon-${iconName}'></i>${data.text}</div>`);
-
-    const popupTemplate = (e) => {
-        let formElement = $('<div>');
-
-        form = formElement.dxForm({
-            formData: rowData,
-            showColonAfterLabel: true,
-            items: [{
-                itemType: 'group',
-                colCount: 2,
-                items: [{
-                    dataField: 'FirstName',
-                    validationRules: validationRules.firstName,
-                    label: {
-                        template: labelTemplate('user')
-                    },
-                }, {
-                    dataField: 'Position',
-                    editorType: 'dxSelectBox',
-                    editorOptions: {
-                        items: positions,
-                        searchEnabled: true,
-                    },
-                    label: {
-                        template: labelTemplate('info')
-                    },
-                }, {
-                    dataField: 'LastName',
-                    validationRules: validationRules.lastName,
-                    label: {
-                        template: labelTemplate('user')
-                    },
-                }, {
-                    dataField: 'Address',
-                    label: {
-                        template: labelTemplate('home')
-                    },
-                }, {
-                    dataField: 'BirthDate',
-                    editorType: 'dxDateBox',
-                    validationRules: validationRules.birthDate,
-                    label: {
-                        template: labelTemplate('event')
-                    },
-                }, { 
-                    dataField: 'HireDate',
-                    editorType: 'dxDateBox',
-                    label: {
-                        template: labelTemplate('event')
-                    },
-                }, {
-                    colSpan: 2,
-                    dataField: 'Notes',
-                    editorType: 'dxTextArea',
-                    editorOptions: {
-                        height: 90,
-                        maxLength: 200
-                    },
-                    label: {
-                        template: (data, element) => {
-                            const lineBreak = '<br>';
-                            const infoIcon = '<i id="helpedInfo" class="dx-icon dx-icon-info"></i>';
-                            const labelText = `Additional${lineBreak}${data.text}`;
-                
-                            element.append(`<div id='template-content'>${infoIcon}${labelText}</div>`)
-                
-                            $('<div>').dxTooltip({
-                                target: '#helpedInfo',
-                                showEvent: 'mouseenter',
-                                hideEvent: 'mouseleave',
-                                contentTemplate(args) {
-                                    args.html('<div id="tooltip-content">This field must not exceed 200 characters</div>')
-                                },
-                            }).appendTo(element);
-                        },
-                    },
-                }, {
-                    dataField: 'Phone',
-                    validationRules: validationRules.phone,
-                    editorOptions: {
-                        mask: '+1 (X00) 000-0000', 
-                        maskRules: { X: /[02-9]/ },
-                        maskInvalidMessage: 'The phone must have a correct USA phone format'
-                    },
-                    label: {
-                        template: labelTemplate('tel')
-                    },
-                }, {
-                    dataField: 'Email',
-                    validationRules: validationRules.email,
-                    label: {
-                        template: labelTemplate('email')
-                    },
-                }],
-            }],
-        }).dxForm('instance');
-    
-        e.append(formElement);
-    };
-
-    const grid = $('#grid').dxDataGrid({
+    const grid = $("#grid").dxDataGrid({
         showBorders: true,
         showColumnLines: true,
         showRowLines: true,
         repaintChangesOnly: true,
-        dataSource:  new DevExpress.data.DataSource({
+        dataSource: new DevExpress.data.DataSource({
             store: {
-                type: 'array',
+                type: "array",
                 data: employees,
-                key: 'ID',
-            }
+                key: "ID",
+            },
         }),
         editing: {
             allowUpdating: true,
             allowAdding: true,
             allowDeleting: true,
             useIcons: true,
-            mode: 'popup'
+            mode: "popup",
         },
         columns: [
-            { dataField: 'FirstName', dataType: 'string' },
-            { dataField: 'LastName', dataType: 'string' },
-            { dataField: 'BirthDate', dataType: 'date' },
-            { dataField: 'Position', dataType: 'string', width: 170 },
-            { dataField: 'HireDate', dataType: 'date' },
-            { dataField: 'Address', dataType: 'string' },
+            { dataField: "FirstName", dataType: "string" },
+            { dataField: "LastName", dataType: "string" },
+            { dataField: "BirthDate", dataType: "date" },
+            { dataField: "Position", dataType: "string", width: 170 },
+            { dataField: "HireDate", dataType: "date" },
+            { dataField: "Address", dataType: "string" },
             {
-                type: 'buttons',
-                buttons: [{
-                    name: 'edit',
-                    onClick: (e) => {
-                        showPopup('Edit', e.row.data)
-                    }
-                }, 'delete']
-            }
+                type: "buttons",
+                buttons: [
+                    {
+                        name: "edit",
+                        onClick: (e) => {
+                            showPopup(false, e.row.data);
+                        },
+                    },
+                    "delete",
+                ],
+            },
         ],
         toolbar: {
             items: [
                 {
-                    widget: 'dxButton',
+                    widget: "dxButton",
                     options: {
-                        icon: 'plus',
+                        icon: "plus",
                         onClick: () => {
-                            showPopup('Add', {})
-                        }
+                            showPopup(true, {});
+                        },
                     },
                 },
             ],
         },
-    }).dxDataGrid('instance');
+    }).dxDataGrid("instance");
 
-    const popup = $('#popup').dxPopup({
-        contentTemplate: popupTemplate,
+    const validationGroupName = "gridForm";
+
+    const createPopupTemplate = (formData) => () => {
+        const labelTemplate = (iconName) => (data) =>
+            $(`<div><i class="dx-icon dx-icon-${iconName}"></i>${data.text}</div>`);
+            return $("<div>").dxForm({
+                formData: formData,
+                validationGroup: validationGroupName,
+                showColonAfterLabel: true,
+                items: [{
+                    itemType: "group",
+                    colCount: 2,
+                    items: [
+                        {
+                            dataField: "FirstName",
+                            validationRules: validationRules.firstName,
+                            label: {
+                                template: labelTemplate("user"),
+                            },
+                        },
+                        {
+                            dataField: "Position",
+                            editorType: "dxSelectBox",
+                            editorOptions: {
+                                items: positions,
+                                searchEnabled: true,
+                            },
+                            label: {
+                                template: labelTemplate("info"),
+                            },
+                        },
+                        {
+                            dataField: "LastName",
+                            validationRules: validationRules.lastName,
+                            label: {
+                                template: labelTemplate("user"),
+                            },
+                        },
+                        {
+                            dataField: "Address",
+                            label: {
+                                template: labelTemplate("home"),
+                            },
+                        },
+                        {
+                            dataField: "BirthDate",
+                            editorType: "dxDateBox",
+                            validationRules: validationRules.birthDate,
+                            label: {
+                                template: labelTemplate("event"),
+                            },
+                        },
+                        {
+                            dataField: "HireDate",
+                            editorType: "dxDateBox",
+                            label: {
+                                template: labelTemplate("event"),
+                            },
+                        },
+                        {
+                            colSpan: 2,
+                            dataField: "Notes",
+                            editorType: "dxTextArea",
+                            editorOptions: {
+                                height: 90,
+                                maxLength: 200,
+                            },
+                            label: {
+                                template: (data, element) => {
+                                    const lineBreak = "<br>";
+                                    const infoIcon =
+                                        `<i id="helpedInfo" class="dx-icon dx-icon-info"></i>`;
+                                    const labelText = `Additional${lineBreak}${data.text}`;
+
+                                    element.append(
+                                        `<div id="template-content">${infoIcon}${labelText}</div>`
+                                    );
+
+                                    $("<div>").dxTooltip({
+                                        target: "#helpedInfo",
+                                        showEvent: "mouseenter",
+                                        hideEvent: "mouseleave",
+                                        contentTemplate(args) {
+                                            args.html(
+                                                `<div id="tooltip-content">This field must not exceed 200 characters</div>`
+                                            );
+                                        },
+                                    }).appendTo(element);
+                                },
+                            },
+                        },
+                        {
+                            dataField: "Phone",
+                            validationRules: validationRules.phone,
+                            editorOptions: {
+                                mask: "+1 (X00) 000-0000",
+                                maskRules: { X: /[02-9]/ },
+                                maskInvalidMessage:
+                                "The phone must have a correct USA phone format",
+                            },
+                            label: {
+                                template: labelTemplate("tel"),
+                            },
+                        },
+                        {
+                            dataField: "Email",
+                            validationRules: validationRules.email,
+                            label: {
+                                template: labelTemplate("email"),
+                            },
+                        },
+                    ],
+                }],
+            });
+    };
+
+    const popup = $("#popup").dxPopup({
         hideOnOutsideClick: true,
         height: 420,
-        toolbarItems: [{
-            widget: 'dxButton',
-            location: 'after',
-            toolbar: 'bottom',
-            options: {
-                text: 'Confirm',
-                type: 'success',
-                onClick: () => {
-                    let result = form.validate();
+    }).dxPopup("instance");
 
-                    if (result.isValid) {
-                        let data = form.option('formData'),
-                            gridSource = grid.getDataSource(),
-                            gridStore = gridSource.store();
+    const confirmItem = {
+        widget: "dxButton",
+        location: "after",
+        toolbar: "bottom",
+        options: {
+            text: "Confirm",
+            type: "success",
+        },
+    };
 
-                        if (formMode === 'Add')
-                            gridStore.insert(data)
-                        else if (formMode === 'Edit')
-                            gridStore.update(data['ID'], data)
+    const cancelItem = {
+        widget: "dxButton",
+        location: "after",
+        toolbar: "bottom",
+        options: {
+            text: "Cancel",
+            onClick: () => {
+                popup.hide();
+            },
+        },
+    };
 
-                        grid.refresh(true)
-                        popup.hide()
-                    }
-                }
-            }
-        }, {
-            widget: 'dxButton',
-            location: 'after',
-            toolbar: 'bottom',
-            options: {
-                text: 'Cancel',
-                toolbar: 'bottom',
-                onClick: () => {
-                    popup.hide()
-                }
-            }
-        }]
-    }).dxPopup('instance');
-
-    const showPopup = (popupMode, data) => {
-        formMode = popupMode;
-
-        if (form)
-            form.option('formData', { ...data })
-        else
-            rowData = { ...data }
-
+    const showPopup = (isNewRecord, data) => {
+        const contentTemplate = createPopupTemplate(data);
         popup.option({
-            title: popupMode,
-            visible: true
-        })
+            title: isNewRecord ? "Add" : "Edit",
+            contentTemplate,
+            height: "auto",
+            toolbarItems: [
+                {
+                    ...confirmItem,
+                    onClick: () => {
+                        let result = DevExpress.validationEngine.validateGroup(validationGroupName);
+                        if (result.isValid) {
+                            const gridSource = grid.getDataSource(),
+                                gridStore = gridSource.store();
+                            if (isNewRecord) {
+                                gridStore.insert(data);
+                            } else {
+                                gridStore.update(data["ID"], data);
+                            }
+                            grid.refresh(true);
+                            popup.hide();
+                        }
+                    },
+                },
+                cancelItem,
+            ],
+            visible: true,
+        });
     };
 });
