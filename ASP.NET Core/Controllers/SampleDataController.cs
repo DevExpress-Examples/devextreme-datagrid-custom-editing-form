c709ab621d453c2f83d3aea0f3c13071b7c864fe
flagmanAndrew <andrey.sergeyev@devexpress.com>
2023-04-17 21:42:35 +0700

    Add ASP

---
+++ ASP.NET Core/Controllers/SampleDataController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Xml.Linq;
+using ASP.NET_Core.Models;
+using ASP_NET_Core.Models;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ASP_NET_Core.Extensions;
+
+namespace ASP_NET_Core.Controllers {
+
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller {
+
+        [HttpGet]
+        public object Get(DataSourceLoadOptions loadOptions) {
+            return DataSourceLoader.Load(EmployeeStore.Employees, loadOptions);
+        }
+        [HttpPost]
+        public IActionResult Add(string values) {
+            var newEmployee = new Employee();
+            JsonConvert.PopulateObject(values, newEmployee);
+
+            if (!TryValidateModel(newEmployee))
+                return BadRequest(ModelState.GetFullErrorMessage());
+
+            EmployeeStore.Employees.Add(newEmployee);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(int key, string values) {
+            var employee = EmployeeStore.Employees.First(a => a.ID == key);
+            JsonConvert.PopulateObject(values, employee);
+
+            if (!TryValidateModel(employee))
+                return BadRequest(ModelState.GetFullErrorMessage());
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public void Delete(int key) {
+            var employee = EmployeeStore.Employees.First(a => a.ID == key);
+            EmployeeStore.Employees.Remove(employee);
+        }
+    }
+}
---
+++ ASP.NET Core/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System;
+
+namespace ASP_NET_Core.Extensions
+{
+    static class Extensions {
+        public static string GetFullErrorMessage(this ModelStateDictionary modelState) {
+            var messages = new List<string>();
+
+            foreach (var entry in modelState) {
+                foreach (var error in entry.Value.Errors)
+                    messages.Add(error.ErrorMessage);
+            }
+
+            return String.Join(" ", messages);
+        }
+    }
+}
