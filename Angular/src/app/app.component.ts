d38ee9c340c4edc710145f2a588c610b325bc665
Tom <meschken@yandex.ru>
2025-06-17 22:19:01 +0300

    lint&refactor

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -2,94 +2,111 @@ import { Component, ViewChild } from '@angular/core';
 import { DxDataGridComponent } from 'devextreme-angular';
 
 import validationEngine from 'devextreme/ui/validation_engine';
-
+import notify from 'devextreme/ui/notify';
 import ArrayStore from 'devextreme/data/array_store';
+import { DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
 import { Employee, Service } from './app.service';
 
-type PositionEditorOptions = {
-    items: string[], 
-    searchEnabled: boolean
+interface PositionEditorOptions {
+  items: string[];
+  searchEnabled: boolean;
 }
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [Employee, Service]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [Service],
 })
 
 export class AppComponent {
-    @ViewChild("grid", {static: false}) grid: DxDataGridComponent;
+  @ViewChild('grid', { static: false }) grid!: DxDataGridComponent;
 
-    employees: Employee[];
-    employeeStore: ArrayStore;
+  employees: Employee[];
 
-    formData: Employee[];
-    isNewRecord = true;
-    visible = false;
+  employeeStore: ArrayStore;
 
-    positionEditorOptions: PositionEditorOptions;
-    notesEditorOptions = { height: 90, maxLength: 200 };
-    phoneEditorOptions = { 
-        mask: "+1 (X00) 000-0000", 
-        maskRules: { X: /[02-9]/ },
-        maskInvalidMessage: "The phone must have a correct USA phone format", 
-    };
+  formData = {} as Employee;
 
-    labelTemplates = [
-        { name: "name", icon: "dx-icon-user" },
-        { name: "position", icon: "dx-icon-info" },
-        { name: "date", icon: "dx-icon-event" },
-        { name: "address", icon: "dx-icon-home" },
-        { name: "phone", icon: "dx-icon-tel" },
-        { name: "email", icon: "dx-icon-email" },
-    ];
+  isNewRecord = true;
 
-    get validationGroupName() { 
-        return "gridForm"; 
-    }
+  visible = false;
 
-    showPopup = (isNewRecord, formData) => {
-        this.formData = formData;
-        this.isNewRecord = isNewRecord;
-        this.visible = true;
-    };
+  positionEditorOptions: PositionEditorOptions;
 
-    hidePopup = () => { 
-        this.visible = false;
-    };
+  notesEditorOptions = { height: 90, maxLength: 200 };
 
-    confirmChanges = () => {
-        const result = validationEngine.validateGroup(this.validationGroupName);
-     
-        if (!result.isValid)
-            return;
+  phoneEditorOptions = {
+    mask: '+1 (X00) 000-0000',
+    maskRules: { X: /[02-9]/ },
+    maskInvalidMessage: 'The phone must have a correct USA phone format',
+  };
 
-        if (this.isNewRecord)
-            this.employeeStore.insert(this.formData); 
-        else 
-            this.employeeStore.update(this.formData["ID"], this.formData);
+  labelTemplates = [
+    { name: 'name', icon: 'dx-icon-user' },
+    { name: 'position', icon: 'dx-icon-info' },
+    { name: 'date', icon: 'dx-icon-event' },
+    { name: 'address', icon: 'dx-icon-home' },
+    { name: 'phone', icon: 'dx-icon-tel' },
+    { name: 'email', icon: 'dx-icon-email' },
+  ];
 
-        this.grid.instance.refresh(true);
-        this.hidePopup();
-    };
+  validationGroupName = 'gridForm';
 
-    addRow = () => {
-        this.showPopup(true, {});
-    };
+  constructor(service: Service) {
+    this.employees = service.getEmployees();
 
-    editRow = (e) => {
-        this.showPopup(false, {...e.row.data});
-    };
+    this.employeeStore = new ArrayStore({
+      data: this.employees,
+      key: 'ID',
+    });
 
-    constructor(service: Service) {
-        this.employees = service.getEmployees();
+    this.positionEditorOptions = { items: service.getPositions(), searchEnabled: true };
+  }
 
-        this.employeeStore = new ArrayStore({
-            data: this.employees,
-            key: "ID",
-        });  
+  showPopup = (isNewRecord: boolean, formData: Employee): void => {
+    this.formData = formData;
+    this.isNewRecord = isNewRecord;
+    this.visible = true;
+  };
 
-        this.positionEditorOptions = { items: service.getPositions(), searchEnabled: true };
-    }
+  hidePopup = (): void => {
+    this.visible = false;
+  };
+
+  confirmChanges = (): void => {
+    const result = validationEngine.validateGroup(this.validationGroupName);
+    if (!result.isValid) return;
+
+    const operation = this.isNewRecord
+      ? this.employeeStore.insert(this.formData)
+      : this.employeeStore.update(this.formData.ID, this.formData);
+
+    operation
+      .then(() => this.grid.instance.refresh(true))
+      .then(() => this.hidePopup())
+      .catch(() => {
+        notify('An error occurred while saving changes.', 'error', 3000);
+      });
+  };
+
+  addRow = (): void => {
+    this.showPopup(true, {
+      ID: 0,
+      FirstName: '',
+      LastName: '',
+      Prefix: '',
+      Position: '',
+      BirthDate: '',
+      HireDate: '',
+      Notes: '',
+      Address: '',
+      Phone: '',
+      Email: '',
+    });
+  };
+
+  editRow = (e: DxDataGridTypes.ColumnButtonClickEvent): void => {
+    this.showPopup(false, { ...e.row?.data });
+  };
 }
--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -3,4 +3,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  /* eslint-disable no-console */
   .catch((err) => console.error(err));
