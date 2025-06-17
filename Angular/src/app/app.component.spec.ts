34a379ecd584709ef1557d9ccb2821dae47ec811
Tom <22076961+artem-kurchenko@users.noreply.github.com>
2025-06-17 22:23:50 +0300

    Fix build errors (#70)
    * update packages
    * remove yarn
    * lint&refactor

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -6,10 +6,10 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DxAngularApp'`, () => {
+  it('should have as title \'DxAngularApp\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('DxAngularApp');
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,39 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DxPopupModule } from "devextreme-angular/ui/popup";
+import { DxPopupModule } from 'devextreme-angular/ui/popup';
 
 import {
-    DxDataGridModule,
-    DxTemplateModule,
-    DxButtonModule,
-} from 'devextreme-angular';
+  DxDataGridModule,
+  DxTemplateModule,
+  DxButtonModule,
 
-import {
-    DxSelectBoxModule,
-    DxTextAreaModule,
-    DxFormModule,
-    DxTooltipModule,
+  DxSelectBoxModule,
+  DxTextAreaModule,
+  DxFormModule,
+  DxTooltipModule,
 } from 'devextreme-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        DxPopupModule,
-        DxSelectBoxModule,
-        DxTextAreaModule,
-        DxFormModule,
-        DxTooltipModule,
-        DxDataGridModule,
-        DxTemplateModule,
-        DxButtonModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    DxPopupModule,
+    DxSelectBoxModule,
+    DxTextAreaModule,
+    DxFormModule,
+    DxTooltipModule,
+    DxDataGridModule,
+    DxTemplateModule,
+    DxButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 
 export class AppModule { }
