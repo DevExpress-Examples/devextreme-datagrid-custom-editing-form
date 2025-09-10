08daec46be904c961a01eb2496c2ae00754940b5
DuckTieCorpMember <danyshbek1996@gmail.com>
2025-09-10 16:20:08 +0500

    Fixed React components

--- React/src/LabelNotesTemplate.tsx
+++ React/src/LabelNotesTemplate.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Tooltip } from 'devextreme-react/tooltip';
 
-const LabelNotesTemplate = (data: any) => {
-    return (
-        <React.Fragment>
-            <div id="template-content">
-                <i id="helpedInfo" className="dx-icon dx-icon-info"></i>
-                Additional
-                <br />
-                { data.text }
-            </div>
+function LabelNotesTemplate(data: any): JSX.Element {
+  return (
+    <React.Fragment>
+      <div id="template-content">
+        <i id="helpedInfo" className="dx-icon dx-icon-info"></i>
+          Additional
+        <br />
+        { data.text }
+      </div>
 
-            <Tooltip
-                target="#helpedInfo"
-                showEvent="mouseenter"
-                hideEvent="mouseleave" 
-            >
-                <div id="tooltip-content">This field must not exceed 200 characters</div>
-            </Tooltip>
-        </React.Fragment>
-    );
+      <Tooltip
+        target="#helpedInfo"
+        showEvent="mouseenter"
+        hideEvent="mouseleave"
+      >
+        <div id="tooltip-content">This field must not exceed 200 characters</div>
+      </Tooltip>
+    </React.Fragment>
+  );
 }
 
 export default LabelNotesTemplate;
--- React/src/LabelTemplate.tsx
+++ React/src/LabelTemplate.tsx
@@ -1,4 +1,20 @@
-const LabelTemplate = (iconName: any) => 
-    (data: any) => (<div><i className={`dx-icon dx-icon-${iconName}`}></i>{data.text}</div>);
+interface LabelTemplateData {
+  [key: string]: any;
+  text?: string;
+}
+
+// eslint-disable-next-line no-unused-vars
+function LabelTemplate(iconName: string): (data: LabelTemplateData) => JSX.Element {
+  function Template(data: LabelTemplateData): JSX.Element {
+    return (
+      <div>
+        <i className={`dx-icon dx-icon-${iconName}`}></i>
+        {data.text}
+      </div>
+    );
+  }
+  (Template as any).displayName = `LabelTemplate(${iconName})`;
+  return Template;
+}
 
 export default LabelTemplate;
